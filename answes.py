466e0cf3ba4764d26c5d984a85d1dda23339bff5
EranStudents <131552121+EranStudents@users.noreply.github.com>
2023-10-04 17:00:08 +0300

    Add files via upload

---
+++ answes.py
@@ -0,0 +1,20 @@
+# שאלה 1
+
+name = 'Eran'
+last_name = 'Levy'
+
+print(name)
+print(last_name)
+
+# שאלה 2
+
+my_name = input('Enter your name :')
+print(my_name)
+
+# שאלה 3
+
+my_name_2 = input('Enter your name :')
+age = int('Enter your age : ')
+
+print(my_name_2)
+print(age)
